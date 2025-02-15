bb21a7a8c4d1a2f2a6421b32a575105ef15c1415
PC-27 <PC-27@DESKTOP-IM26562>
2025-02-15 12:57:41 +0900

    ""

---
+++ BasicJava/src/kr/or/ddit/study01/DocExample.java
@@ -0,0 +1,29 @@
+package kr.or.ddit.study01;
+
+
+/**
+ *  alt + shift + j
+ */
+public class DocExample {
+	public static void main(String[] args) {
+		
+		// 한줄 주석
+		// ctrl +shift + c 
+
+		/*
+		 *  여러줄 주석 
+		System.out.println("주석 테스트");
+		System.out.println("주석 테스트");
+		System.out.println("주석 테스트");
+		System.out.println("주석 테스트");
+		*/
+		
+		System.out.println("주석 테스트");
+		System.out.println("주석 테스트");
+		System.out.println("주석 테스트");
+		System.out.println("주석 테스트");
+		
+	}
+}
+
+
---
+++ BasicJava/src/kr/or/ddit/study02/sec02/CharExample.java
@@ -0,0 +1,27 @@
+package kr.or.ddit.study02.sec02;
+
+public class CharExample {
+	public static void main(String[] args) {
+		// char : 부호 없는 2byte 정수(0~65535)
+		//        문자 표현을 위한 정수
+		
+		char c1 = 97;
+		System.out.println(c1);
+		
+		char c2 = 66;
+		System.out.println(c2);
+		
+		char c3 = '가';
+		
+		int i3 = c3+0;
+		
+		System.out.println(i3);
+		
+		char c4 = 44032;
+		
+		System.out.println(c4);
+				
+		
+		
+	}
+}
